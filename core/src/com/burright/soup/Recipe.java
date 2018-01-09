f87a16fb2a60479f26011cc5e2ea2c1d47e167a5
Sean Burright <sean.burright@gmail.com>
2018-01-08 21:29:58 -0800

    added Recipe

---
+++ core/src/com/burright/soup/Recipe.java
@@ -0,0 +1,12 @@
+package com.burright.soup;
+
+public class Recipe
+{
+    private final int CORN = 1;
+    private final int MEAT = 2;
+    private final int BROCCOLI = 3;
+    private final int POTATO = 4;
+    private final int MARBLE = 5;
+    private final int EGG = 6;
+    private final int SPIDER = 7;
+}
