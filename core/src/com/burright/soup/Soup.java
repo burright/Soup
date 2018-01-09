80f30cf12bb20c8647ceea3daadc43ea4c9eb938
Sean Burright <sean.burright@gmail.com>
2018-01-09 08:02:42 -0800

    flushing out

--- core/src/com/burright/soup/Soup.java
+++ core/src/com/burright/soup/Soup.java
@@ -1,25 +1,30 @@
 package com.burright.soup;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Soup extends ApplicationAdapter {
+public class Soup extends Game
+{
 
 	@Override
-	public void create () {
+	public void create ()
+    {
 
 	}
 
 	@Override
-	public void render () {
+	public void render ()
+    {
 
 	}
 	
 	@Override
-	public void dispose () {
+	public void dispose ()
+    {
 
 	}
 }
